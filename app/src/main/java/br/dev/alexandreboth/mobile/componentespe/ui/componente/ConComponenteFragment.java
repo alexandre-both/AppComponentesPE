0c59eb66cecfe4ef91f0a6413dd39f5d3e567d51
alexandre-both <alexandre.b04@aluno.ifsc.edu.br>
2023-10-30 15:16:56 -0300

    terminei consultas no java

--- app/src/main/java/br/dev/alexandreboth/mobile/componentespe/ui/componente/ConComponenteFragment.java
+++ app/src/main/java/br/dev/alexandreboth/mobile/componentespe/ui/componente/ConComponenteFragment.java
@@ -12,11 +12,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -78,27 +84,73 @@ public class ConComponenteFragment extends Fragment implements Response.ErrorLis
         //inicializando a fila de requests do SO
         this.requestQueue.start();
 
-        // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
-            recyclerView.setAdapter(new ConComponenteRecyclerViewAdapter(componentes));
-        }
+        //array parametro de envio para o serviço
+        JSONArray jsonArray = new JSONArray();
+        //objeto com informações de filtro da consulta
+
+        Componente componente = new Componente();
+        componente.setId(1);
+        componente.setNome("IRF8010");
+
+        //incluindo objeto no array de envio
+        jsonArray.put(componente.toJsonObject());
+        //requisição para o Rest Server
+        jsonArrayReq = new JsonArrayRequest(Request.Method.GET,
+                "http://10.0.2.2/concomp.php",
+                jsonArray, this, this);
+        //mando executar a requisção na fila do sistema
+        requestQueue.add(jsonArrayReq);
+
         return this.view;
     }
 
     @Override
     public void onErrorResponse(VolleyError error) {
-
+        //mostrar mensagem que veio do servidor
+        Snackbar mensagem = Snackbar.make(view,
+                "Ops! Houve um problema ao realizar a consulta: " + error.toString(), Snackbar.LENGTH_LONG);
+        mensagem.show();
     }
 
     @Override
     public void onResponse(Object response) {
 
+        try {
+            //array Json para receber a resposta do webservice
+            JSONArray jsonArray = null;
+            jsonArray = new JSONArray(response.toString());
+            //se a consulta não veio vazia passar para array list
+            if (jsonArray != null) {
+                //objeto java
+                Componente componente = null;
+                //array list para receber a resposta
+                this.componentes = new ArrayList<Componente>();
+                //preenchendo ArrayList com JSONArray recebido
+                for (int i = 0, size = jsonArray.length(); i < size; i++) {
+                    JSONObject jo = jsonArray.getJSONObject(i);
+                    componente = new Componente(jo);
+                    this.componentes.add(componente);
+                }
+                // Set the adapter
+                if (view instanceof RecyclerView) {
+                    Context context = view.getContext();
+                    RecyclerView recyclerView = (RecyclerView) view;
+                    if (mColumnCount <= 1) {
+                        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+                    } else {
+                        recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+                    }
+                    recyclerView.setAdapter(new ConComponenteRecyclerViewAdapter(componentes));
+                }
+            } else {
+                Snackbar mensagem = Snackbar.make(view,
+                        "A consulta não retornou nenhum registro!",
+                        Snackbar.LENGTH_LONG);
+                mensagem.show();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
+
